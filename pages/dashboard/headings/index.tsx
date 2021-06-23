04b8918d46f8570d9914758c440b4d6f2df2bac7
Dejan <33637358+Dejan85@users.noreply.github.com>
2021-06-23 22:59:25 +0200

    added headings page

---
+++ pages/dashboard/headings/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HeadingsPage = () => {
+  return <div>Headings</div>;
+};
+
+export default HeadingsPage;
--- src/components/frontend/navigation/Navigation.tsx
+++ src/components/frontend/navigation/Navigation.tsx
@@ -14,75 +14,31 @@ const Navigation: React.FC = (): JSX.Element => {
           <Link href="/dashboard/title" passHref>
             <LinkWrapper>Title</LinkWrapper>
           </Link>
-
-          {/* <Ul>
-            <Li>
-              <Link href="/dashboard/title/add-title" passHref>
-                <LinkWrapper>Add Title</LinkWrapper>
-              </Link>
-            </Li>
-            <Li>
-              <Link href="/dashboard/title/add-title" passHref>
-                <LinkWrapper>Edit Title</LinkWrapper>
-              </Link>
-            </Li>
-          </Ul> */}
         </Li>
 
         <Li>
+          <Link href="/dashboard/headings" passHref>
+            <LinkWrapper>Headings</LinkWrapper>
+          </Link>
+        </Li>
+
+        {/* <Li>
           <Link href="/dashboard/blog" passHref>
             <LinkWrapper>Blog</LinkWrapper>
           </Link>
-
-          {/* <Ul>
-            <Li>
-              <Link href="/dashboard/blog/add-blog" passHref>
-                <LinkWrapper>Add Blog</LinkWrapper>
-              </Link>
-            </Li>
-            <Li>
-              <Link href="/dashboard/blog/edit-blog" passHref>
-                <LinkWrapper>Edit Blog</LinkWrapper>
-              </Link>
-            </Li>
-          </Ul> */}
         </Li>
 
         <Li>
           <Link href="/dashboard/events" passHref>
             <LinkWrapper>Events</LinkWrapper>
           </Link>
-          {/* <Ul>
-            <Li>
-              <Link href="/dashboard/events/add-events" passHref>
-                <LinkWrapper>Add Events</LinkWrapper>
-              </Link>
-            </Li>
-            <Li>
-              <Link href="/dashboard/events/edit-events" passHref>
-                <LinkWrapper>Edit Events</LinkWrapper>
-              </Link>
-            </Li>
-          </Ul> */}
         </Li>
 
         <Li>
           <Link href="/dashboard/about" passHref>
             <LinkWrapper>About</LinkWrapper>
           </Link>
-          {/* <Ul>
-            <Li>
-              <Link href="/dashboard/events/add-about" passHref>
-                <LinkWrapper>Add About</LinkWrapper>
-              </Link>
-            </Li>
-            <Li>
-              <Link href="/dashboard/About/edit-about" passHref>
-                <LinkWrapper>Edit About</LinkWrapper>
-              </Link>
-            </Li>
-          </Ul> */}
-        </Li>
+        </Li> */}
       </Ul>
     </Nav>
   );
